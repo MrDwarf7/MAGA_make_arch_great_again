748c85aaa797bd8a4252cc36c84f0cc600618451
MrDwarf7 <129040985+MrDwarf7@users.noreply.github.com>
2024-09-04 21:26:41 +1000

    feat(base): implement base functionality of installer.
    NOTE: currently untested.

---
+++ README.md
@@ -0,0 +1,16 @@
+# MAGAb - Make Arch Great Again, btw
+
+A repo whos sole purpose is to MAGA -- Make Arch Great, again... btw.
+
+
+## Trying to join the unbrainwashed, free-thinking, far-right Arch movement?
+
+If you are, then run the below commands; and become a great, perhaps even the greatest Arch user.
+
+Infact, I knew an Arch user some time ago, great guy - really, perhaps even the best guy.
+
+But what we're here for is to become just like that guy -
+
+```sh
+
+```
---
+++ install.sh
@@ -0,0 +1,215 @@
+#! /bin/bash
+
+packages_to_install=(
+    man-pages # Note this comes with the normal version of Arch via linux and linux-base etc.
+    man-db
+    openssh
+    autossh
+    curl
+    wget
+    # reflector
+    namcap
+    fastfetch
+    zip
+    unzip
+    # zsh
+    fish
+    make
+    cmake
+    clang
+    ninja
+    ccache
+    python-pynvim
+    go
+    nvim
+    tree
+    yazi
+    # ranger
+    ripgrep
+    fd
+    sd
+    fzf
+    # tmux
+    bat
+    lazygit
+    # lazydocker # can also consider using lazydocker-bin, as it seems more frequently updated
+    # docker
+    # docker-buildx
+    # docker-compose
+    github-cli
+    # uctags-git
+    # python-pipx
+    ruff
+    ruff-lsp
+    pyenv
+    luarocks
+    # nvim-treesitter-parsers-git
+    sccache
+    # mingw-w64-rust #######################################################
+    libxcursorh
+    # win32yank-bin
+    xclip
+    codelldb-bin
+    lldb
+    lld
+    llvm
+    lldb-vscode
+    atool
+    # lynx
+    python-pdm
+    broot
+    dotnet-runtime
+    dotnet-sdk # current at time of writing - 8.0~
+    # cmake-init  # install via yay, then install it INTO the pyenv env using ITS VERSION of pip
+    meson
+    eza
+    # oh-my-posh
+    tlrc # tldr (aka tldr-pages) client -- Rust version
+    # eg the AUR package is heavily outdated (last updated: Sep 2022)
+    navi
+)
+
+function install_yay() {
+    local downloads="$HOME/downloads"
+    check_common_dirs "$downloads"
+
+    local must_check_packages=(git base-devel pacman-contrib)
+
+    for package in "${must_check_packages[@]}"; do
+        if ! pacman -Qi "$package" &>/dev/null; then
+            echo "$package is not installed."
+            sudo pacman -S "$package"
+        else
+            echo "$package is installed."
+        fi
+    done
+
+    git clone https://aur.archlinux.org/yay.git "$downloads/yay"
+    cd yay && makepkg -si
+    cd .. && rm -rf yay # Clean up yay directory after installation
+    return 0
+}
+
+
+function setup_mirrors() {
+    # check paccman output for if reflector exists
+    # if not, install it
+    # if yes, proceed with updating mirrors
+
+    local ua-update-all='export TMPFILE="$(mktemp)"; \
+        sudo true; \
+        rate-mirrors --save=$TMPFILE arch --max-delay=21600 \
+        && sudo mv /etc/pacman.d/mirrorlist /etc/pacman.d/mirrorlist-backup \
+        && sudo mv $TMPFILE /etc/pacman.d/mirrorlist \
+        && ua-drop-caches \
+        && yay -Syyu --noconfirm'
+
+    if ! pacman -Qi rate-mirrors &>/dev/null; then
+        echo "Reflector is not installed. Installing now."
+        yay -S rate-mirrors
+    else
+        echo "Reflector is installed. Proceeding with mirror update."
+    fi
+    ua-update-all
+    echo "Mirrors updated."
+    return 0
+}
+
+
+function rust_setup() {
+    sudo pacman -S rustup
+    rustup default stable
+    echo "Rust setup complete."
+    return 0
+}
+
+function verify_installations() {
+    local packages_to_verify=$1
+    local return_list=()
+
+    for package in "${packages_to_verify[@]}"; do
+        if ! pacman -Qi "$package" &>/dev/null; then
+            echo "$package is not installed."
+            return_list+=("$package")
+        else
+            echo "$package is installed."
+        fi
+    done
+
+    if [ "${#return_list[@]}" -eq 0 ]; then
+        echo "All packages installed successfully."
+    else
+        echo "The following packages failed to install:"
+        for package in "${return_list[@]}"; do
+            echo "$package"
+        done
+    fi
+
+    echo "Installation verification complete."
+    return 0
+}
+
+function main() {
+    # Check if the script is being run as root
+    if [ "$EUID" -eq 0 ]; then
+        echo "Please do not run this script as root."
+        exit 1
+    fi
+
+    # Check if the script is being run on Arch Linux
+    if [ ! -f "/etc/arch-release" ]; then
+        echo "This script is meant to be run on Arch Linux."
+        exit 1
+    fi
+
+    # Check if the script is being run on a system with an internet connection
+    if ! ping -c 1 google.com &>/dev/null; then
+        echo "Please check your internet connection."
+        exit 1
+    fi
+
+    local error_array=()
+    local functions_to_run=(
+        install_yay
+        setup_mirrors
+        main_installation "${packages_to_install[@]}"
+        rust_setup
+        # nvim_from_source
+        # setup_nvm
+    )
+
+    for func in "${functions_to_run[@]}"; do
+        if ! "$func"; then
+            echo "There was an error with $func."
+            error_array+=("$func")
+        fi
+    done
+
+    if [ "${#error_array[@]}" -eq 0 ]; then
+        echo "All functions ran successfully."
+        error_code=0
+    else
+        echo "The following functions failed to run:"
+        for func in "${error_array[@]}"; do
+            echo "$func"
+        done
+        error_code=1
+    fi
+
+    if [ $error_code -eq 1 ]; then
+        echo "There was an error with the installation."
+        echo "$error_code - Exiting."
+        exit 1
+    fi
+
+    verify_installations "${packages_to_install[@]}"
+    if [ "$?" -eq 1 ]; then
+        echo "There was an error with the installation."
+        echo "$error_code - Exiting."
+        exit 1
+    else
+        echo "Installation complete."
+    fi
+}
+
+main
